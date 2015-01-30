e31dc3b7e3a15143c09915e7414b044433ecdaab
Lukasz Siwinski <lsiwinski@gmail.com>
2015-01-30 22:49:31 +0100

    gs

--- panning.rb
+++
@@ -1,8 +0,0 @@
-# mono left channel
-play :C5, pan: -1
-sleep 1
-# stereo
-play :C5, pan: 0
-sleep 1
-# mono right channel
-play :C5, pan: 1
--- synth-params-pan.rb
+++ synth-params-pan.rb
@@ -5,3 +5,10 @@ sleep 1
 play 60, pan: 0 # center
 sleep 1
 play 60, pan: 1 # right
+
+
+play :C5, pan: -1 # mono left channel
+sleep 1
+play :C5, pan: 0 # stereo
+sleep 1
+play :C5, pan: 1 # mono right channel
