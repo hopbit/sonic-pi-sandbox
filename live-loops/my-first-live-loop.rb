17be26e852a76f8de4ecb483fa21fcc34210498f
Lukasz Siwinski <lsiwinski@gmail.com>
2015-04-25 00:47:40 +0200

    my first live loop

---
+++ live-loops/my-first-live-loop.rb
@@ -0,0 +1,14 @@
+# my first live loop 
+live_loop :kick do
+  sample :bd_haus, rate: 0.5
+  sleep 0.5
+end
+live_loop :syntsh do
+  sleep 0.25
+#  sample :ambi_choir, rate: 0.4
+  #sample :bd_haus, rate: 7
+  sleep 0.25
+  #sample :ambi_choir, rate: 0.7
+ # sample :bd_haus, rate: 10
+
+end
