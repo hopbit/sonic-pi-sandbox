c4d26a87e414d41e45d37276eec98b1f540abcfe
Lukasz Siwinski <lsiwinski@gmail.com>
2015-09-12 22:48:09 +0200

    Sample Pack Buffer

---
+++ buffers/buffer_03.rb
@@ -0,0 +1,7 @@
+# Sample Packs
+
+#use_sample_pack "D:/hopbit/dev/workspace/workshops/overtone-workshop/resources"
+#use_sample_pack "D:/hopbit/dev/workspace/workshops/overtone-workshop/resources/daft"
+#use_sample_pack "D:/hopbit/dev/workspace/workshops/overtone-workshop/resources/dubstep"
+#use_sample_pack "D:/hopbit/dev/workspace/workshops/overtone-workshop/resources/house"
+#use_sample_pack "D:/hopbit/dev/workspace/workshops/overtone-workshop/resources/lights"
